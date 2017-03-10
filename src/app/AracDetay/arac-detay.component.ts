5a8e6a5b597be91d551980739a927ab25b1910dc
Ahmet Elçik <ahmetelcik35@gmail.com>
2017-03-10 23:36:07 +0300

    araç detay sağ taraf yapildi

---
+++ src/app/AracDetay/arac-detay.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-arac-detays',
+  templateUrl: './arac-detay.component.html',
+})
+export class AracDetayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
