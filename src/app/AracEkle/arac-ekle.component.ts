1ff0bfc2bc5e1d7494accc3e8de3bd1409cea640
Ahmet Elçik <ahmetelcik35@gmail.com>
2017-03-13 01:54:20 +0200

    araç ekle component eklendi

---
+++ src/app/AracEkle/arac-ekle.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-arac-ekle',
+  templateUrl: './arac-ekle.component.html',
+})
+export class AracEkleComponent implements OnInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+  }
+
+}
