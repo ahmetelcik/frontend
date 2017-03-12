f6e88e40c6e650c7b333461d882c6ca0e7bc398f
Ahmet Elçik <ahmetelcik35@gmail.com>
2017-03-12 03:04:56 +0200

    kiralama detay  html ve js dosyası yapildi

---
+++ src/app/KiralamaDetay/kiralama-detay.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kiralama-detay',
+  templateUrl: './kiralama-detay.component.html',
+})
+export class KiralamaDetayComponent implements OnInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+  }
+
+}
