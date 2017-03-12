62006195f376d056aa7b8621141f511792969218
Ahmet Elçik <ahmetelcik35@gmail.com>
2017-03-12 08:35:32 +0300

    gider detay component eklendi

---
+++ src/app/GiderDetay/gider-detay.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gider-detay',
+  templateUrl: './gider-detay.component.html',
+})
+export class GiderDetayComponent implements OnInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+  }
+
+}
