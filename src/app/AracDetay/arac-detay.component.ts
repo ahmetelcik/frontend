9046fb547be8d1f40eb24211c411cf0b2459a203
Ahmet Elçik <ahmetelcik35@gmail.com>
2017-03-21 22:28:02 +0300

    delete

--- src/app/AracDetay/arac-detay.component.ts
+++ src/app/AracDetay/arac-detay.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ModalDirective } from 'ng2-bootstrap/modal';
 import { AracDetayGosterComponent } from './Components/arac_detay.component';
 import { AracNotlariListeleComponent } from './Components/Arac_notlari_listele.component';
+
+
 @Component({
   selector: 'app-arac-detays',
   templateUrl: './arac-detay.component.html',
@@ -13,11 +15,6 @@ export class AracDetayComponent implements OnInit {
   }
 
 
-
-  /* Araç Sil Click Event */
-  aracSilEvent(id:number){
-    console.log("araç siliniyor " + id);
-  }
 
 
   ngOnInit() {
