c8611c5eb535a772a475b1d14f0aa9c6a9b448c9
Ahmet Elçik <ahmetelcik35@gmail.com>
2017-03-27 12:42:48 +0300

    edit

--- src/app/MusteriDuzenle/Components/musteri_duzenle_form.component.ts
+++ src/app/MusteriDuzenle/Components/musteri_duzenle_form.component.ts
@@ -94,9 +94,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MusteriDuzenleFormComponent implements OnInit {
     
-    
     constructor() { }
 
+    
     ngOnInit() { }
 
     
